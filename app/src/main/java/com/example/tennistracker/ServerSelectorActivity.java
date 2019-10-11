08de3dd26f63a8f3b462d9512abeb47390343050
Damienguyen <damien-nguyen@hotmail.fr>
2019-10-11 19:17:29 +0200

    -- initial commit --

--- app/src/main/java/com/example/tennistracker/ServerSelectorActivity.java
+++ app/src/main/java/com/example/tennistracker/ServerSelectorActivity.java
@@ -7,12 +7,37 @@ import android.view.View;
 import android.widget.Button;
 
 public class ServerSelectorActivity extends AppCompatActivity {
+    int i =0;
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, ServerSelectorActivity.class);
+
+
+        intent.putExtra("MY_Key", i);
+        startActivity(intent);
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_selector);
+        Button bt1 = findViewById(R.id.button);
+        Button bt2 = findViewById(R.id.button2);
+
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = 1;
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = 2;
+            }
+        });
 
 
     }
